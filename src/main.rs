a8e670801fe4fe52831f00416b0c2ac6b9b5a8e3
valentine-mario <valentine13400@gmail.com>
2020-07-28 11:53:05 +0100

    added test framework

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,31 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
 
 use core::panic::PanicInfo;
 mod vga_buffer;
+mod serial;
 
 // This function is called on panic.
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
 }
 
+// our panic handler in test mode
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failed);
+    loop {}
+}
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
@@ -18,6 +33,64 @@ pub extern "C" fn _start() -> ! {
     vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
     write!(vga_buffer::WRITER.lock(), ", some numbers: {} {}", 42, 1.337).unwrap();
     println!("Hello World{}", "!");
+    #[cfg(test)]
+    test_main();
 //    panic!("panic message");
     loop {}
+}
+
+#[test_case]
+fn trivial_assertion() {
+    assert_eq!(1, 1);
+
+}
+
+
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Testable]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        serial_println!("Running {} tests", tests.len());
+        //since tests implements the testatble traint
+        test.run();
+
+        //qmeu exit after successful test
+        exit_qemu(QemuExitCode::Success);
+    }
+}
+
+// update our test_runner to print these messages automatically.
+pub trait Testable {
+    fn run(&self) -> ();
+}
+
+impl<T> Testable for T
+where
+    T: Fn(),
+{
+    fn run(&self) {
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        //self implements the fn() trait
+        self();
+        serial_println!("[ok]");
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    //success and failure code
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        //write to the iobase 0xf4 of isa-debug-exit and exit
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
 }
---
+++ tests/basic_boot.rs
@@ -0,0 +1,30 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+#![test_runner(mini_os::test_runner)]
+
+
+use core::panic::PanicInfo;
+use mini_os::println;
+use mini_os::test_runner;
+
+#[no_mangle] // don't mangle the name of this function
+pub extern "C" fn _start() -> ! {
+    test_main();
+
+    loop {}
+}
+
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    mini_os::test_panic_handler(info)
+}
+
+
+#[test_case]
+fn test_println() {
+    println!("test_println output");
+}
